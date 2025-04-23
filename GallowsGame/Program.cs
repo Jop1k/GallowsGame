c59d47860451ae6f5d1334d0ed8f13d57b7174e4
Jop1k <cgggeghhg@gmail.com>
2025-04-23 17:13:19 +0800

    Add project files.

---
+++ GallowsGame/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new GallowsGame();
+
+            while (true)
+            {
+                Console.WriteLine("Выбирайте: 1) Играть | 2) Выйти");
+                string userAnswer = Console.ReadLine().ToLower();
+
+                switch (userAnswer)
+                {
+                    case "1":
+                    case "играть":
+                        game.PlayGame();
+                        Console.Clear();
+                        break;
+                    case "2":
+                    case "выйти":
+                        return;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Такого выбора нет");
+                        break;
+                }
+            }
+
+        }
+    }
+}
